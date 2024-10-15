d009744a7cdba20af8e2af7e5a7812f68b65b1c5
Alvaro Rodriguesgit config --global user.email alvarordgs98@gmail.com <alvarordgs98@gmail.com>
2024-10-14 21:18:18 -0300

    creating jwt authentication configÂ

--- src/main/java/br/com/buscapetapi/buscapetapi/model/User.java
+++ src/main/java/br/com/buscapetapi/buscapetapi/model/User.java
@@ -6,16 +6,19 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
-public class User {
-
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -44,7 +47,6 @@ public class User {
     @Column(name = "reset_token", length = 150)
     private String resetToken;
 
-
 //    @OneToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "id")
 //    private int address;
@@ -59,4 +61,34 @@ public class User {
     @Column(name = "updated_at")
     private LocalDate updatedAt;
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
