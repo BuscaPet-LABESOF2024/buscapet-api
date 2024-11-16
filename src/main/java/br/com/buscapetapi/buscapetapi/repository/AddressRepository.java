1b368e550869ef2b55f9a92f29fdfc9de76b3088
ANA CRISTINA THIERSCH DA CRUZ <ana.thiersch@gmail.com>
2024-11-15 21:51:33 -0300

    feat: add get neighborhoods end-point

--- src/main/java/br/com/buscapetapi/buscapetapi/repository/AddressRepository.java
+++ src/main/java/br/com/buscapetapi/buscapetapi/repository/AddressRepository.java
@@ -2,8 +2,14 @@ package br.com.buscapetapi.buscapetapi.repository;
 
 import br.com.buscapetapi.buscapetapi.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    @Query("SELECT DISTINCT a.neighborhood FROM Address a")
+    List<String> findDistinctNeighborhoods();
 }
