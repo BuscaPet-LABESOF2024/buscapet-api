7d4704e83c1f35835fa9ba4d13740552a2aec332
MarllonBruno20 <marllon.bruno2002@gmail.com>
2024-12-16 16:37:25 -0300

    Ajustando para filtrar apenas pelos anúncios ativos

--- src/main/java/br/com/buscapetapi/buscapetapi/repository/AnnouncementRepository.java
+++ src/main/java/br/com/buscapetapi/buscapetapi/repository/AnnouncementRepository.java
@@ -1,7 +1,9 @@
 package br.com.buscapetapi.buscapetapi.repository;
 
-import br.com.buscapetapi.buscapetapi.dto.output.AnnouncementOutput;
 import br.com.buscapetapi.buscapetapi.model.Announcement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -18,4 +20,14 @@ public interface AnnouncementRepository extends JpaRepository<Announcement, Long
 
     List<Announcement> findByUserId(Long userId);
 
+    List<Announcement> findAllByActiveTrue(Sort sort);
+
+    @Query("SELECT a FROM Announcement a WHERE a.active = true")
+    List<Announcement> findActive(Sort sort);
+
+    @Query("SELECT a FROM Announcement a WHERE a.active = true")
+    Page<Announcement> findActive(Pageable pageable);
+
+    @Query("SELECT a FROM Announcement a WHERE a.active = true")
+    Page<Announcement> findActive(Specification<Announcement> and, Pageable page);
 }
